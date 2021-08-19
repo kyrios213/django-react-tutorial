d870efb25dcbc0b5a20cacba529d4626b879a659
Lance Pidor <kyrios213@gmail.com>
2021-08-19 18:26:52 +0800

    Room page

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('room/', views.RoomView.as_view(), name='room'),
     path('create-room/', views.CreateRoomView.as_view(), name='create-room'),
+    path('get-room', views.GetRoomView.as_view(), name='get-room'),
 ]
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import CreateRoomPage from "./CreateRoomPage";
 import RoomJoinPage from "./RoomJoinPage";
+import Room from "./Room";
 import {
   BrowserRouter as Router,
   Switch,
@@ -23,6 +24,7 @@ export default class HomePage extends Component {
           </Route>
           <Route path="/join" component={RoomJoinPage}></Route>
           <Route path="/create" component={CreateRoomPage}></Route>
+          <Route path="/room/:roomCode" component={Room}></Route>
         </Switch>
       </Router>
     );
