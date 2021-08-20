e0cfe1eb4238839418e01420e06a93cff6d8aa9c
Lance Pidor <kyrios213@gmail.com>
2021-08-20 17:56:11 +0800

    Join Room Page

--- api/urls.py
+++ api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('room/', views.RoomView.as_view(), name='room'),
     path('create-room/', views.CreateRoomView.as_view(), name='create-room'),
     path('get-room', views.GetRoomView.as_view(), name='get-room'),
+    path('join-room', views.JoinRoomView.as_view(), name='join-room'),
 ]
